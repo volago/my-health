f86ac70a68ba8d1a84a814e112c200e5d771afa7
walter.luszczyk <walter.luszczyk@ceneo.pl>
2025-05-14 21:34:10 +0200

    shared and domain nx libs

---
+++ libs/domain/src/lib/domain/domain.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-domain',
+  imports: [CommonModule],
+  templateUrl: './domain.component.html',
+  styleUrl: './domain.component.css',
+})
+export class DomainComponent {}
---
+++ libs/shared/src/lib/shared/shared.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-shared',
+  imports: [CommonModule],
+  templateUrl: './shared.component.html',
+  styleUrl: './shared.component.css',
+})
+export class SharedComponent {}
