bc20cd97a6b6f997c722a018f30e5d1ab2083a65
walter.luszczyk <walter.luszczyk@ceneo.pl>
2025-05-21 22:22:32 +0200

    nx projects configuration

--- libs/features/catalog/eslint.config.mjs
+++ libs/features/catalog/eslint.config.mjs
@@ -26,7 +26,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'lib',
+          prefix: 'myHealth',
           style: 'camelCase',
         },
       ],
@@ -34,7 +34,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'lib',
+          prefix: 'my-health',
           style: 'kebab-case',
         },
       ],
